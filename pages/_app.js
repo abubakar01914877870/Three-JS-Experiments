361481f786b9b2193100a1b7e09a8aacf4693d36
abubakar01914877870 <abubakar01914877870@gmail.com>
2024-10-10 22:41:14 +0600

    delete this files

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,8 @@
 import "../styles/globals.css";
-
-// import layout
 import Layout from "../components/Layout";
-// import { VercelToolbar } from "@vercel/toolbar/next";
-
-// import next js router
 import { useRouter } from "next/router";
-
-// import framer motion
 import { AnimatePresence } from "framer-motion";
-// import { useEffect, useState } from "react";
-// import Loading from "../components/Loading";
-// import { LocationProvider } from "../context/location-context";
+
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
