afee5de1f3287a90385a02e52fd7a20fd6f2745e
abubakar01914877870 <abubakar01914877870@gmail.com>
2024-10-10 22:50:27 +0600

    remove files and update

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
   swcMinify: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-        pathname: "/**",
-      },
+      // {
+      //   protocol: "https",
+      //   hostname: "cdn.sanity.io",
+      //   port: "",
+      //   pathname: "/**",
+      // },
       {
         protocol: "https",
         hostname: "images.pexels.com",
@@ -30,16 +30,16 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    SANITY_AUTH_TOKEN: process.env.SANITY_AUTH_TOKEN,
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
-    SANITY_API_VERSION: process.env.SANITY_API_VERSION,
-    MAXMIND_ACCOUNT_ID: process.env.MAXMIND_ACCOUNT_ID,
-    MAXMIND_LICENSE_KEY: process.env.MAXMIND_LICENSE_KEY,
-    IMAGE_SOURCE: process.env.IMAGE_SOURCE,
-    FETCH_LOCATION: process.env.FETCH_LOCATION,
-  },
+  // env: {
+  //   SANITY_AUTH_TOKEN: process.env.SANITY_AUTH_TOKEN,
+  //   SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+  //   SANITY_DATASET: process.env.SANITY_DATASET,
+  //   SANITY_API_VERSION: process.env.SANITY_API_VERSION,
+  //   MAXMIND_ACCOUNT_ID: process.env.MAXMIND_ACCOUNT_ID,
+  //   MAXMIND_LICENSE_KEY: process.env.MAXMIND_LICENSE_KEY,
+  //   IMAGE_SOURCE: process.env.IMAGE_SOURCE,
+  //   FETCH_LOCATION: process.env.FETCH_LOCATION,
+  // },
 };
 
 module.exports = nextConfig;
